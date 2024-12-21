fd9404715c69fc7d83441da83f7b82d4f118e4a1
tasnimebj <166241671+tasnimebj@users.noreply.github.com>
2024-12-21 21:33:40 +0100

    ..

---
+++ src/main/java/com/example/studentsystem/model/Student.java
@@ -0,0 +1,17 @@
+package com.example.studentsystem.model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue
+
+    private int student_id;
+    private String student__Fname;
+    private String student_Lname;
+    private Boolean is_absent;
+    private int branch_id;
+
+}
